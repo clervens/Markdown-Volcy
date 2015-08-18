27f442b9b864c22820985493b48c1e31ae8a6a2e
Clervens Volcy <clervens.volcy@gmail.com>
2015-08-17 22:12:41 -0400

    MOD : move the application menu template in his own file

--- main.js
+++ main.js
@@ -18,43 +18,7 @@ app.on('ready', function(){
   mainWindow.loadUrl('file://'+ __dirname + '/index.html');
   mainWindow.openDevTools();
 
-  var template = [{
-    label: "Application",
-    submenu: [
-      { label: "Preferences", enabled: false },
-      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-      { type: "separator" },
-      { label: "Quit", accelerator: "CmdOrCtrl+Q", click: function() { app.quit(); }}]
-    }, {
-      label: "File", submenu: [
-        { label: "Save", accelerator: "CmdOrCtrl+S", enabled: false, click: function(){
-            BrowserWindow.getFocusedWindow().webContents.send('file-save');
-        } },
-        { type: "separator", sublabel: "Debug" }//,
-        // { label: "Debug", enabled: false},
-        // { label: "Reload", accelerator: "CmdOrCtrl+R", click: function() { mainWindow.restart();}},
-        // { label: "Tests Trigger", accelerator: "CmdOrCtrl+T", click: function() { 
-        //   BrowserWindow.getFocusedWindow().webContents.send('test');}}
-      ]
-    }, {
-    label: "Edit",
-    submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-      { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }]
-    }, {
-      label: "View", submenu: []
-    }, {
-      label: "Actions", submenu: []
-    }, {
-      label: "Window", submenu: []
-    }, {
-      label: "Help", submenu: []
-  }];
+  var template = require(__dirname+'/lib/menus/main_menu')(app, BrowserWindow, mainWindow);
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
   mainWindow.on('closed', function() {
