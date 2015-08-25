db032512e1ec52548e3f7c5cfa849e8b9ef0ae12
Clervens Volcy <clervens.volcy@gmail.com>
2015-08-24 20:53:20 -0400

    ADD : app_root global variable

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var app = require('app');
 var BrowserWindow = require('browser-window');
 var ipc = require('ipc');
-global.storage = require(__dirname+'/lib/storage/storage');
-global.i18n = require(__dirname+'/lib/i18n');
+global.app_root = __dirname+'/app';
+global.storage = require(app_root+'/lib/storage/storage');
+global.i18n = require(app_root+'/lib/i18n');
 var environment = storage.get('environment');
 require('crash-reporter').start();
 
@@ -17,11 +18,11 @@ app.on('window-all-closed', function(){
 });
 app.on('ready', function(){
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://'+ __dirname + '/index.html');
+  mainWindow.loadUrl('file://'+ app_root +'/views/index.html');
   if (environment == 'development')
     mainWindow.openDevTools();
 
-  var template = require(__dirname+'/lib/menus/main_menu')(app, BrowserWindow, mainWindow);
+  var template = require(app_root+'/lib/menus/main_menu')(app, BrowserWindow, mainWindow);
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
   mainWindow.on('closed', function() {
